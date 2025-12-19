4c06015e27e67a610c7e56965948d6c8742510ac
Raymond Harsono Alim <raymondalim2@gmail.com>
2025-12-19 14:38:18 +0700

    Create remonoswelcome.sh

---
+++ kustomisasi/remonoswelcome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+zenity --info \
+--title="Selamat datang di Remon OS" \
+--width=600 \
+--no-wrap \
+--text="
+                                                
+                         \e[32m60d00d\e[0m                           
+                          \e[32m0    0v\e[0m                         
+                           \e[32m6d   6\e[0m                         
+                              \e[32m066m\e[0m                        
+                            \e[33mmmmmmmmm\e[0m                       
+   AAAAAAAAAAN        \e[33mmmmmmmmmmm\e[0m        AAAAAAAAv    
+   AAA      AAAd    \e[33mmmmmmmmmmmmmm\e[0m     AAA       AAA    
+   AAA        AA    \e[33mmmmmmmmmmmmmmmm\e[0m   mAA        AA
+   AAA        AA   \e[33mmmmmmmmmmmmmmmmm\e[0m    AI            
+   AAA      IAA1   \e[33mmmmmmmmmmmmmmmmm\e[0m     AAAAAAAv       
+   AAAAAAAAAAE     \e[33mmmmmmmmmmmmmmmmm\e[0m          IAAAA  
+   AAA     AA      \e[33mmmmmmmmmmmmmmmm\e[0m              AAA  
+   AAA      AAv    \e[33mmmmmmmmmmmmmm\e[0m     AAI        RAA   
+   AAA      vAAv    \e[33mmmmmmmmmmmr\e[0m       AAA      AAA    
+   AAA        AAm    \e[33mmmmmmmm\e[0m            AAAAAAAAv     
+                                                          
+
+Selamat datang di Remon OS
+
+Aturan:
+• Akademik only
+• Jangan ubah sistem
+• Simpan data di media eksternal
+"
